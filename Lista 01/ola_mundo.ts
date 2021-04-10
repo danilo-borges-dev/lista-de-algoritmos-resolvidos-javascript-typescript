f0f58aa513f9cf704851b8822d98cf6e2c606810
Danilo Borges <danilosilva2017@hotmail.com>
2021-04-10 14:15:30 -0300

    Primeiro Exercício

--- Lista 01/ola_mundo.ts
+++ Lista 01/ola_mundo.ts
@@ -1,1 +1,6 @@
-// 
+// Imprima no Console a mensagem "Olá Mundo!" utilizando a sintaxe TypeScrpt, porém atribua a String a uma variável;
+
+let mensagem: string = "Olá mundo!";
+
+console.log(`${mensagem}`);
+
